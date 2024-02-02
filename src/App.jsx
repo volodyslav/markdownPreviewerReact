b9cae94331d55b03666e98d97fc4f60d25a0f1e0
Volodyslav <122315136+volodyslav@users.noreply.github.com>
2024-02-02 17:33:34 +0200

    modification v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react"
+
 const App = () => {
+  const [text, setText] = useState("")
+  const [expandEditor, setExpandEditor] = useState(true)
+  const [expandPreviewer, setExpandPreviewer] = useState(true)
+
+  const editorExpand = () => {
+    setExpandEditor(!expandEditor)
+  }
+  const previewerExpand = () => {
+    setExpandPreviewer(!expandPreviewer)
+  }
+  
   return (
     <div className='center'>
+    {expandPreviewer &&
       <div className="editor-box">
-        <div className="editor-title">Editor</div>
-        <textarea id="editor"></textarea>
-      </div>
+        <div className="editor-title"><p>Editor</p> <button onClick={() => editorExpand()} className="editor-expand">
+        {expandEditor ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrows-angle-expand" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707"/>
+            </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrows-angle-contract" viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707M15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707"/>
+      </svg>}
+        
+</button></div>
+        <textarea onChange={(e) => setText(e.target.value)} value={text} className={`${expandEditor ? "editor" : "editor-expanded"}`} id="editor"></textarea>
+      </div>}
+
+      {expandEditor && 
       <div id="preview-box">
-        <div className="preview-title">Previewer</div>
-        <div className="preview-text" id="preview"></div>
-      </div>
+        <div className="preview-title"><p>Previewer</p><button className="editor-expand" onClick={() => previewerExpand()}>
+        {expandPreviewer ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrows-angle-expand" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707"/>
+            </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrows-angle-contract" viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707M15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707"/>
+      </svg>}
+      </button>
+        </div>
+        <div id="preview" className={` ${expandEditor ? "preview-text" : "preview-text-expanded"}`}>{text}</div>
+      </div>}
     </div>
   )
 }
